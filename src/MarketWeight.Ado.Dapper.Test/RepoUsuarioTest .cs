1d80fc47537d1efe90c1203135b4bec71bb8e777
Dieguitoo06 <156547073+Dieguitoo06@users.noreply.github.com>
2025-07-01 09:42:58 -0300

    Asserts

--- src/MarketWeight.Ado.Dapper.Test/RepoUsuarioTest .cs
+++ src/MarketWeight.Ado.Dapper.Test/RepoUsuarioTest .cs
@@ -12,6 +12,7 @@ public class RepoUsuarioTest : TestBase
         => _repo = new RepoUsuario(Conexion);
     
     [Fact]
+
     public void TraerOK()
     {
         var usuarios = _repo.Obtener();
@@ -22,16 +23,22 @@ public class RepoUsuarioTest : TestBase
     }
 
     [Fact]
+
     public void IngresarDineroOK()
     {
         _repo.Ingreso(1, 7707m);
-
         _repo.Ingreso(2, 420m);
-
         _repo.Ingreso(3, 5000m);
-
         _repo.Ingreso(4, 6666m);
-
+        
+        var usuario1 = _repo.Detalle(1);
+        var usuario2 = _repo.Detalle(2);
+        var usuario3 = _repo.Detalle(3);
+        var usuario4 = _repo.Detalle(4);
+        Assert.True(usuario1 != null && usuario1.Saldo >= 7707m);
+        Assert.True(usuario2 != null && usuario2.Saldo >= 420m);
+        Assert.True(usuario3 != null && usuario3.Saldo >= 5000m);
+        Assert.True(usuario4 != null && usuario4.Saldo >= 6666m);
     }
 
     [Fact]
@@ -68,22 +75,30 @@ public class RepoUsuarioTest : TestBase
             Email = "carloselbello@gmail.com",
             Password = "carlos123"
         };
-
-
         _repo.Alta(usuarioWalter);
         _repo.Alta(usuarioJorge);
         _repo.Alta(usuarioGuido);
         _repo.Alta(usuarioCarlos);
+
+        var usuarios = _repo.Obtener();
+        
+        Assert.Contains(usuarios, u => u.Email == "waltercoocker@gmail.com");
+        Assert.Contains(usuarios, u => u.Email == "JorgeCasco@gmail.com");
+        Assert.Contains(usuarios, u => u.Email == "guidopepin@gmail.com");
+        Assert.Contains(usuarios, u => u.Email == "carloselbello@gmail.com");
     }
 
     [Fact]
     public void ComprarMonedaOK()
     {
         _repo.Compra(3, 0.5m, 2);
-
         _repo.Compra(2, 1m, 3);
-
         _repo.Compra(2, 0.5m, 1);
+        
+        var monedasUsuario3 = _repo.ObtenerPorCondicionUsuarioMoneda(3, null);
+        var monedasUsuario2 = _repo.ObtenerPorCondicionUsuarioMoneda(2, null);
+        Assert.NotEmpty(monedasUsuario3);
+        Assert.NotEmpty(monedasUsuario2);
     }
 
     [Fact]
@@ -97,6 +112,8 @@ public class RepoUsuarioTest : TestBase
     public void VenderMonedaOK()
     {
         _repo.Vender(2, 0.1m, 1);
+        var monedasUsuario2 = _repo.ObtenerPorCondicionUsuarioMoneda(2, null);
+        Assert.NotNull(monedasUsuario2);
     }
 
     [Fact]
@@ -160,96 +177,4 @@ public class RepoUsuarioTest : TestBase
 
     }
 
-    [Fact]
-    public async Task TraerOKAsync()
-    {
-        var usuarios = await _repo.ObtenerAsync();
-        Assert.NotEmpty(usuarios);
-        Assert.Contains(usuarios, m => m.Nombre == "Ana");
-    }
-
-    [Fact]
-    public async Task IngresarDineroOKAsync()
-    {
-        await _repo.IngresoAsync(1, 7707m);
-        await _repo.IngresoAsync(2, 420m);
-        await _repo.IngresoAsync(3, 5000m);
-        await _repo.IngresoAsync(4, 6666m);
-    }
-
-    [Fact]
-    public async Task AltaUsuarioOKAsync()
-    {
-        Usuario usuarioWalter = new()
-        {
-            Nombre = "Walte",
-            Apellido = "Beníte",
-            Email = "waltercoocker@gmail.com",
-            Password = "314159265358979"
-        };
-        Usuario usuarioJorge = new()
-        {
-            Nombre = "Jorge",
-            Apellido = "Casco",
-            Email = "JorgeCasco@gmail.com",
-            Password = "jorge123"
-        };
-        Usuario usuarioGuido = new()
-        {
-            Nombre = "Guido",
-            Apellido = "Gavilán",
-            Email = "guidopepin@gmail.com",
-            Password = "guidopepin123"
-        };
-        Usuario usuarioCarlos = new()
-        {
-            Nombre = "Carlos",
-            Apellido = "Bello",
-            Email = "carloselbello@gmail.com",
-            Password = "carlos123"
-        };
-        await _repo.AltaAsync(usuarioWalter);
-        await _repo.AltaAsync(usuarioJorge);
-        await _repo.AltaAsync(usuarioGuido);
-        await _repo.AltaAsync(usuarioCarlos);
-    }
-
-    [Fact]
-    public async Task ComprarMonedaOKAsync()
-    {
-        await _repo.CompraAsync(3, 0.5m, 2);
-        await _repo.CompraAsync(2, 1m, 3);
-        await _repo.CompraAsync(2, 0.5m, 1);
-    }
-
-    [Fact]
-    public async Task VenderMonedaOKAsync()
-    {
-        await _repo.VenderAsync(2, 0.1m, 1);
-    }
-
-    [Fact]
-    public async Task ObtenerPorCondicionOKAsync()
-    {
-        var usuarios = await _repo.ObtenerPorCondicionAsync("saldo >= 1000");
-        Assert.NotEmpty(usuarios);
-    }
-
-    [Fact]
-    public async Task DetalleCompletoOKAsync()
-    {
-        var usuario = await _repo.DetalleCompletoAsync(1);
-        Assert.NotNull(usuario);
-    }
-
-    [Fact]
-    public async Task DetalleCompletoBilleteraOKAsync()
-    {
-        var usuario = await _repo.DetalleCompletoAsync(2);
-        Assert.NotNull(usuario);
-        Assert.NotNull(usuario.Billetera);
-        Assert.NotNull(usuario.Transacciones);
-        Assert.NotEmpty(usuario.Billetera);
-        Assert.NotEmpty(usuario.Transacciones);
-    }
 }
